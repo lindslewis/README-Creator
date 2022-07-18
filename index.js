f4e7af985b962a6e89aae9aaa209ed72cb600656
LindsayLew <lmschwehr@gmail.com>
2022-07-17 22:48:52 -0700

    created questions. started code to write the file. don't think it is right.

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 const inquirer = require('inquirer');
 
 // Create an array of questions for user input
-//TODO:added questions, however, I'm wondering if the below needs to be laid out differently. 1. either to fit better with the const questions, or to reformat to be inquirer.prompt([questions inside here like below])???? 
-const questions = [
+//TODO:added questions, however, I'm wondering if the below needs to be laid out differently. 1. either to fit better with the const questions, or to reformat to be inquirer.prompt([questions inside here like below]) have both for now
+
+// array of questions
+const questions = ["What is the title of your project?","Please give a brief description of your project.", "Please describe the steps for installation for your intended users.", "Please give a concise description on the intended usage of your project.", "Please describe what the guidelines are for those who may wish to contribute to your project.", "Please describe the steps necessary in order to test your project."]
+
+// inquirer for prompt of same questions above.
+inquirer.prompt([
     {
         message: 'What is the title of your project?',
         type: 'input',
@@ -34,10 +39,16 @@ const questions = [
         type: 'input',
         name: 'test',
     }
-];
+]);
+
+// input questions for the contact info
+const contactInfo = ["What is your Github username?", "What is your email address?"]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log(responses)
+    fs.writeFile(`./output/${responses.name}.json`, JSON.stringify(responses,null,))
+}
 
 // TODO: Create a function to initialize app
 function init() {}
