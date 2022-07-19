e736bf039774c6c6b9b5a3cfdcfd41df14393a0a
LindsayLew <lmschwehr@gmail.com>
2022-07-18 18:50:08 -0700

    building template

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // Create an array of questions for user input
 //TODO:added questions, however, I'm wondering if the below needs to be laid out differently. 1. either to fit better with the const questions, or to reformat to be inquirer.prompt([questions inside here like below]) have both for now
+
+
+// README template below:
+
+const generateREADME = (title, description, install, usage, guidelines, test, license, github, email) =>
+    ``
+
 
 // array of questions
 // const questions = ["What is the title of your project?","Please give a brief description of your project.", "Please describe the steps for installation for your intended users.", "Please give a concise description on the intended usage of your project.", "Please describe what the guidelines are for those who may wish to contribute to your project.", "Please describe the steps necessary in order to test your project."]
@@ -40,8 +48,15 @@ inquirer.prompt([
         name: 'test',
     },
     {
+        message: "Please list any resources you may have used or referred to.",
+        type: 'list',
+        name: 'credits',
+        
+    },
+    {
         message: "Which license would you like to use?",
         type: 'checkbox',
+        name: 'license',
         choices: ["Apache License 2.0", "GNU GPLv3", "MIT", "ISC", "Unilicense", "Boost Software License", "Mozilla Public License 2.0"],
     },
     {
@@ -55,13 +70,11 @@ inquirer.prompt([
         name: 'email',
     }
     // below is my understanding for taking those responses and making a file from it??
-]).then(responses=>{
-    fs.writeFile(`./output/${responses.name}.json`, JSON.stringify(responses,null)),(err, data)=> { 
-        if(err){
-            throw err
-        }
-        console.log("yay!");
-    }});
+]).then((responses)=>{
+        console.log(responses);
+        const content = generateREADME (responses);
+        fs.writeFile('userREADME.md', content, (err)=> err ? console.log(err) : console.log('Yay! You made a README!'));
+})
 
 
 
--- template.md
+++ template.md
@@ -6,9 +6,10 @@ ${responses.description}
 ## Table of Contents
 1. [Installation](#Installation)
 2. [Usage](#Usage)
-3.[Contribution Guidelines](#Contribution-Guidelines)
-4.[Testing](#Testing)
+3. [Contribution Guidelines](#Contribution-Guidelines)
+4. [Testing](#Testing)
 5. [Licensing](#Licensing)
+6. [Credits](#Credits)
 
 ## Installation
 ${responses.install}
@@ -17,8 +18,12 @@ ${responses.install}
 ${responses.usage}
 
 ## Contribution Guidelines
+${responses.guidelines}
 
 ## Testing
+${responses.test}
 
 ## Licensing
+${responses.license}
 
+## Credits
