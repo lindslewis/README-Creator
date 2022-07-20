4b28fe2c79993e15af6a39175719d265afadf9f6
LindsayLew <lmschwehr@gmail.com>
2022-07-19 22:22:11 -0700

    finishing up

--- README.md
+++ README.md
@@ -12,15 +12,22 @@ Utilizing node.js and inquirer, users are able to use command lines in their ter
 6. [Credits](#Credits)
 
 ## Installation
-
+Simply make sure you have the index.js file containing this code in your workspace. Also make sure to have inquirer installed by typing ``npm i inquirer`` into your terminal, once you have navigated to your desired directory location. From there, simply type ``node index.js`` in order to begin the prompts to write your README.
 
 ## Usage
 This is intended for use by software and web developers who want to minimize their time drafting their README files, and more time pushing new content in their code. With the use of this template and prompt system, developers can create consistent and efficient README's in a few clicks of a button. 
 
 ## Contribution Guidelines
+Currently this project is not open to contribution, however, this may change in the future.
 
 ## Testing
+The testing for this project is not through any sort of test files, but those are currently in the works, so currently, make sure you console.log often when adding new content in order to track that the changes are processing currectly.
 
 ## Licensing
+No licensing at this time.
 
 ## Credits
+1. Shields.io
+2. MDN Web Docs
+3. Request-Response The Full Stack Blog: Professional README Guide
+4. w3schools
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const renderBadge = {};
+const licenseLink = {};
+const section = {};
+
 
 
 
@@ -15,7 +18,9 @@ const renderBadge = {};
 // README template below:
 const generateREADME = (responses) =>
 `# ${responses.title}
-${responses.license}
+
+${renderBadge.text}
+
 ## Description
 ${responses.description}
 
@@ -26,6 +31,7 @@ ${responses.description}
 4. [Testing](#Testing)
 5. [Licensing](#Licensing)
 6. [Credits](#Credits)
+7. [Questions](#Questions)
 
 ## Installation
 ${responses.install}
@@ -42,10 +48,15 @@ ${responses.test}
 ## Licensing
 I have elected to use ${responses.license}.
 
-${responses.license}
+${section.message}
     
 ## Credits
-${responses.credits}`
+${responses.credits}
+
+## Questions
+If you have any further questions or comments, please feel free to reach out via my Github Account: [${responses.github}](https://github.com/${responses.github}).
+You can also reach me via [email](${responses.email}). 
+`
 
 // inquirer for prompts for userinputs.
 inquirer.prompt([
@@ -113,50 +124,65 @@ inquirer.prompt([
 
     // below is my understanding for taking those responses and making a file from it??
 ]).then((responses)=>{
-        console.log(responses);
-        let data = generateREADME(responses);
+        // console.log(responses);
+        inputResponse(responses);
+        // let data = generateREADME(responses)
+    });
 
-        // const data = generateREADME(responses);
+    const inputResponse = (responses) => {
+        console.log(responses);
+        badge(responses);
+        link(responses);
+        licenseSection(responses);
+        let data = generateREADME(responses);
         fs.writeFile(`./output/${responses.title}README.md`, data,
-    (err)=>  err ? console.log(err) : console.log('Yay! You made a README!'));
+    (err)=>  err ? console.log(err) : console.log('Yay! You made a README!'))
+    }
+        // const data = generateREADME(responses);
+    //     fs.writeFile(`./output/${responses.title}README.md`, data,
+    // (err)=>  err ? console.log(err) : console.log('Yay! You made a README!'));
 
     const badge = (responses) => {
         // function badge(license) {
             if(responses.license === "Apache License 2.0") {
-              return '![Apache](https://img.shields.io/badge/license-Apache%202.0-blue)';
+                renderBadge.text = '![Apache](https://img.shields.io/badge/license-Apache%202.0-blue)'
+            //   return '![Apache](https://img.shields.io/badge/license-Apache%202.0-blue)';
             } if(responses.license === "MIT") {
-              return '![MIT](https://img.shields.io/badge/license-MIT-blue)';
+                renderBadge.text =  '![MIT](https://img.shields.io/badge/license-MIT-blue)'
+            //   return '![MIT](https://img.shields.io/badge/license-MIT-blue)';
             } if (responses.license === "General Public License (GPL)") {
-              return '![GPL](https://img.shields.io/badge/license-GPL-blue)'
+                renderBadge.text = '![GPL](https://img.shields.io/badge/license-GPL-blue)'
+            //   return '![GPL](https://img.shields.io/badge/license-GPL-blue)'
             } if (responses.license === "Mozilla Public License 2.0 (MPL)") {
-              return '![MPL](https://img.shields.io/badge/license-MPL%202.0-blue)'
+                renderBadge.text = '![MPL](https://img.shields.io/badge/license-MPL%202.0-blue)'
+            //   return '![MPL](https://img.shields.io/badge/license-MPL%202.0-blue)'
             } if (responses.license === "none") {
-              return ' '
+              renderBadge.text = '';
             } else {
-              return ' '
+             renderBadge.text = '';
             }
         }
-        let licenseLink = (responses) => {
+    const link = (responses) => {
         // function link(license) {
-            if(responses.license === "Apache License 2.0") {
-              return '![Apache License 2.0](https://opensource.org/licenses/Apache-2.0)';
-            } if (responses.license === "MIT") {
-              return '![MIT](https://opensource.org/licenses/MIT)';
-            } if (responses.license === "General Public License (GPL)") {
-              return '![General Public License](https://opensource.org/licenses/gpl-license)'
-            } if (responses.license === "Mozilla Public License 2.0 (MPL)") {
-              return '![Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)'
-            } if (responses.license === "none") {
-              return ' '
-            } else {
-              return ' '
-            }
+        if(responses.license === "Apache License 2.0") {
+            licenseLink.message ='![Apache License 2.0](https://opensource.org/licenses/Apache-2.0)'
+            // return '![Apache License 2.0](https://opensource.org/licenses/Apache-2.0)';
+        } if (responses.license === "MIT") {
+            licenseLink.message = '![MIT](https://opensource.org/licenses/MIT)';
+        } if (responses.license === "General Public License (GPL)") {
+            licenseLink.message = '![General Public License](https://opensource.org/licenses/gpl-license)'
+        } if (responses.license === "Mozilla Public License 2.0 (MPL)") {
+            licenseLink.message = '![Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)'
+        } if (responses.license === "none") {
+            licenseLink.message = ' '
+        } else {
+            licenseLink.message = ' '
         }
+    }
+    const licenseSection = (responses) => {
         
-        let licenseSection = (responses) => {
-        // function licenseSection(license) {
             if (responses.license === "Apache License 2.0"){
-              return `Copyright ${responses.year} ${responses.name}
+              section.message = `Copyright ${responses.year} ${responses.name}
           
               Licensed under the Apache License, Version 2.0 (the "License");
               you may not use this file except in compliance with the License.
@@ -170,8 +196,7 @@ inquirer.prompt([
               See the License for the specific language governing permissions and
               limitations under the License.`;
             } if (responses.license === "MIT") {
-              return `Copyright (c) ${responses.year} ${responses.name}
-          
+                section.message = `Copyright (c) ${responses.year} ${responses.name}
               Permission is hereby granted, free of charge, to any person obtaining a copy
               of this software and associated documentation files (the "Software"), to deal
               in the Software without restriction, including without limitation the rights
@@ -190,7 +215,7 @@ inquirer.prompt([
               OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
               SOFTWARE.`;
             } if (responses.license === "General Public License (GPL)") {
-              return `
+                section.message = `
               Copyright (C) ${responses.year} ${responses.name}
               
               This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
@@ -199,150 +224,11 @@ inquirer.prompt([
               
               You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA`
             } if (responses.license === "Mozilla Public License 2.0 (MPL)") {
-              return `This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.`
+                section.message = `This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.`
             } if (responses.license === "none") {
-              return ' '
+                section.message = ' '
             } else {
-              return ' '
+                section.message =' '
             }
           }
-
-        //   const license = licenseLink.concat(licenseSection);
-})
-
-
-// const license = (responses.license);
-
-// maybe I need to append them together???
-
-// let badge = (license) => {
-// // function renderLicenseBadge(license) {
-//     if(license === "Apache License 2.0") {
-//       return '![Apache](https://img.shields.io/badge/license-Apache%202.0-blue)';
-//     } if(license === "MIT") {
-//       return '![MIT](https://img.shields.io/badge/license-MIT-blue)';
-//     } if (license === "General Public License (GPL)") {
-//       return '![GPL](https://img.shields.io/badge/license-GPL-blue)'
-//     } if (license === "Mozilla Public License 2.0 (MPL)") {
-//       return '![MPL](https://img.shields.io/badge/license-MPL%202.0-blue)'
-//     } if (license === "none") {
-//       return ' '
-//     } else {
-//       return ' '
-//     }
-// }
-// let licenseLink = (license) => {
-// // function renderLicenseLink(license) {
-//     if(license === "Apache License 2.0") {
-//       return '![Apache License 2.0](https://opensource.org/licenses/Apache-2.0)';
-//     } if (license === "MIT") {
-//       return '![MIT](https://opensource.org/licenses/MIT)';
-//     } if (license === "General Public License (GPL)") {
-//       return '![General Public License](https://opensource.org/licenses/gpl-license)'
-//     } if (license === "Mozilla Public License 2.0 (MPL)") {
-//       return '![Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)'
-//     } if (license === "none") {
-//       return ' '
-//     } else {
-//       return ' '
-//     }
-// }
-
-// let licenseSection = (license) => {
-// // function renderLicenseSection(license) {
-//     if (license === "Apache License 2.0"){
-//       return `Copyright ${responses.year} ${responses.name}
-  
-//       Licensed under the Apache License, Version 2.0 (the "License");
-//       you may not use this file except in compliance with the License.
-//       You may obtain a copy of the License at
-   
-//         http://www.apache.org/licenses/LICENSE-2.0
-   
-//       Unless required by applicable law or agreed to in writing, software
-//       distributed under the License is distributed on an "AS IS" BASIS,
-//       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//       See the License for the specific language governing permissions and
-//       limitations under the License.`;
-//     } if (license === "MIT") {
-//       return `Copyright (c) ${responses.year} ${responses.name}
-  
-//       Permission is hereby granted, free of charge, to any person obtaining a copy
-//       of this software and associated documentation files (the "Software"), to deal
-//       in the Software without restriction, including without limitation the rights
-//       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//       copies of the Software, and to permit persons to whom the Software is
-//       furnished to do so, subject to the following conditions:
-      
-//       The above copyright notice and this permission notice shall be included in all
-//       copies or substantial portions of the Software.
-      
-//       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//       SOFTWARE.`;
-//     } if (license === "General Public License (GPL)") {
-//       return `
-//       Copyright (C) ${responses.year} ${responses.name}
-      
-//       This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-      
-//       This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-      
-//       You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA`
-//     } if (license === "Mozilla Public License 2.0 (MPL)") {
-//       return `This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.`
-//     } if (license === "none") {
-//       return ' '
-//     } else {
-//       return ' '
-//     }
-//   }
-  
-// const init = () => {
-//     promptQuestions()
-//     .then((responses => fs.writeFileSync('userREADME.md', generateREADME(responses))))
-//     .then(()=> console.log("Good job!"))
-//     .catch((err) => console.log(err))
-// }
-
-// init();
-
-// Create a function to write README file
-
-// function writeToFile(fileName, data) {
-//     console.log(responses)
-//     fs.writeFile(`./output/${responses.name}.json`, JSON.stringify(responses,null,))
-// }
-
-// TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
-
-
-
-// User input needed:
-// description of project
-// installation instructions for project
-// usage information
-// contribution guidelines
-// test instructions
-// user input also needed: for contact purposes
-// github username
-// email address
-
-// user choose license information out of list
-    // does the license choice happen in here, the index, or in the generate markdown js???
-    // license options:
-        // apache license 2.0, GNU GPLv3, MIT, ISC, unilicense, boost softward license, mozilla public license 2.0
-// relevant badge for license is added to readme
-// section in readme explains the license it's covered under
-
-// save user responses
-// attach user responses to their relevant sections
 
